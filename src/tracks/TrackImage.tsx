db8e452c57d57fdef989fc5ace4518a9fc5710fe
Tim Kye <tim@kye.dev>
2023-06-02 12:01:32 -0700

    feat: center images and align control panel

--- src/tracks/TrackImage.tsx
+++ src/tracks/TrackImage.tsx
@@ -5,29 +5,29 @@ import ImageEditor from './ImageEditor.tsx'
 
 export default function TrackItem(props: { track: Track; isFinal?: boolean }) {
   return (
-    <div class="flex flex-col max-w-full gap-y-2 mb-4">
-      <div class="flex flex-none h-[30px]  justify-between">
-        <Button
-          small
-          secondary
-          disabled={props.track.position === 0}
-          onClick={() => moveTrack(props.track, props.track.position - 1)}
-        >
-          ←
-        </Button>
-        <Button small danger onClick={() => deleteTrack(props.track.id)}>
-          Delete
-        </Button>
-        <Button
-          small
-          secondary
-          disabled={props.isFinal}
-          onClick={() => moveTrack(props.track, props.track.position + 1)}
-        >
-          →
-        </Button>
-      </div>
-      <div class="flex-none">
+    <div class="flex justify-center max-w-full gap-y-2 mb-4">
+      <div class="flex-shrink relative">
+        <div class="flex h-[30px] justify-between mb-2">
+          <Button
+            small
+            secondary
+            disabled={props.track.position === 0}
+            onClick={() => moveTrack(props.track, props.track.position - 1)}
+          >
+            ←
+          </Button>
+          <Button small danger onClick={() => deleteTrack(props.track.id)}>
+            Delete
+          </Button>
+          <Button
+            small
+            secondary
+            disabled={props.isFinal}
+            onClick={() => moveTrack(props.track, props.track.position + 1)}
+          >
+            →
+          </Button>
+        </div>
         <ImageEditor
           src={props.track.image}
           alt={props.track.id}
